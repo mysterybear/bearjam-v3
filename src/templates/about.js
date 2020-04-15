23e20bde7001f29c7f05dd488d11a9904d2149b3
Tom <tom@bearjam.dev>
2020-04-15 15:15:16 +0100

    templated about page

--- src/templates/about.js
+++ src/templates/about.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import Presence from '../components/Presence';
-import { motion } from 'framer-motion';
 import { defaultVariants } from '../animations';
+import Card from "../components/Card";
+import { IconGitHub, IconInstagram, IconLinkedIn, IconTwitter } from "../components/icons";
+import Presence from '../components/Presence';
+import SvgMagAvatar from '../components/SvgMagAvatar';
+import SvgTomAvatar from '../components/SvgTomAvatar';
+
+const icons = {
+  twitter: IconTwitter,
+  github: IconGitHub,
+  instagram: IconInstagram,
+  linkedin: IconLinkedIn
+}
+
+const avatars = {
+  Magda: SvgMagAvatar,
+  Tom: SvgTomAvatar
+}
 
 const AboutTemplate = ({
+  frontmatter,
   children
 }) => {
   return (
-    <Presence key="aboutPage">
-      <motion.div {...defaultVariants}>
-        <p>real about shittt...</p>
-      </motion.div>
+    <Presence key="aboutPage" {...defaultVariants}>
+      <div>
+        {frontmatter.cards.map(({
+          name,
+          email,
+          body,
+          social
+        }) => (
+            <Card
+              avatar={avatars[name]}
+              name={name}
+              email={email}
+              socialLinks={social.map(({ site, url }) => ({ Icon: icons[site], url }))}
+              body={body}
+            />
+          ))}
+      </div>
     </Presence>
   );
 }
