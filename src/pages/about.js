175218f6ae811bdd310767044cdf9e7862955358
magdairis <magdapetford@gmail.com>
2020-04-15 11:18:16 +0100

    added card

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../components/Card";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>sweet and lovely</h1>
+      <Card></Card>
+    </div>
+  );
+}
+
+export default AboutPage;
