64cf600181e161fb0d327241465c7114b08250d5
magdairis <magdapetford@gmail.com>
2020-04-17 12:47:00 +0100

    form style

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,50 +15,62 @@ const CheckBox = forwardRef(({
     </div>
   ))
 
+const Label = ({
+  children,
+  ...otherProps
+}) => {
+  return (
+    <label className="block mb-1 font-bold" {...otherProps}>{children}</label>
+  )
+}
+
 const ContactPage = () => {
   const { register, handleSubmit, watch } = useForm();
   const onSubmit = data => console.log(data);
 
   return (
-    <Presence key="contactPage" {...defaultVariants}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mt-4 ml-2 font-bold">
-          <label htmlFor="name" className="ml-1">Name</label>
-          <input type="text" id="name" name="name" className="border-2 rounded-md bg-gray-100" name="Name" ref={register({ required: true, maxLength: 50 })} />
+    <Presence key="contactPage" className="mt-8" {...defaultVariants}>
+      <h1 className="text-lg text-center">Say hello!</h1>
+      <h4 className="text-center">Get a free 1-to-1 consultation</h4>
+      <form onSubmit={handleSubmit(onSubmit)} className="border-2 rounded-lg border-gray-200 shadow-lg p-4 max-w-xs mt-4">
+        <div className="mt-4">
+          <Label htmlFor="name">Name</Label>
+          <input type="text" id="name" name="name" className="w-full border-2 rounded-md bg-gray-100" name="Name" ref={register({ required: true, maxLength: 50 })} />
         </div>
-        <div className="mt-4 ml-2 font-bold">
-          <label htmlFor="email" className="ml-1">Email</label>
-          <input type="email" id="email" name="email" className="border-2 rounded-md bg-gray-100" name="Email" ref={register({ required: true })} />
+        <div className="mt-4">
+          <Label htmlFor="email">Email</Label>
+          <input type="email" id="email" name="email" className="w-full border-2 rounded-md bg-gray-100" name="Email" ref={register({ required: true })} />
         </div>
-        <div className="mt-4 ml-2 font-bold">
-          <label htmlFor="need" className="ml-1">What do you need?</label>
+        <div className="mt-8">
+          <Label htmlFor="need">What do you need?</Label>
+
+          <div className="flex flex-wrap justify-center mt-2">
+            {[
+              { name: "website", label: "Website" },
+              { name: "mobileApp", label: "Mobile App" },
+              { name: "webApp", label: "Web App" },
+              { name: "branding", label: "Branding" },
+              { name: "other", label: "Other" },
+            ].map(ps => (
+              <CheckBox
+                {...ps}
+                key={ps.name}
+                ref={register}
+                className={cx('transition duration-150 ease-in-out border-2 rounded-md shadow-sm py-1 px-2 m-1 inline-block text-sm', watch()[ps.name] ? 'bg-red-500' : 'bg-transparent')}
+              />
+            ))}
+          </div>
         </div>
 
-        <div className="flex flex-wrap border-2 border-red-500 justify-center max-w-xs">
-          {[
-            { name: "website", label: "Website" },
-            { name: "mobileApp", label: "Mobile App" },
-            { name: "webApp", label: "Web App" },
-            { name: "branding", label: "Branding" },
-            { name: "other", label: "Other" },
-          ].map(ps => (
-            <CheckBox
-              {...ps}
-              key={ps.name}
-              ref={register}
-              className={cx('transition duration-150 ease-in-out border-2 rounded-md shadow-md p-1 m-1 inline-block', watch()[ps.name] ? 'bg-red-500' : 'bg-transparent')}
-            />
-          ))}
+        <div className="mt-8">
+          <Label htmlFor="message">Tell us more</Label>
+          <textarea id="message" name="message" className="border-2 rounded-md bg-gray-100 block w-full h-24 resize-none" />
+        </div>
+        <div className="flex justify-center mt-4">
+
+          <input type="submit" value="Send" className="border-2 rounded-md py-1 px-2 bg-pink-400 shadow-sm text-white font-bold tracking-wide" />
         </div>
 
-        <textarea name="description" ref={register({ required: true })} />
-        <div className="border-2 rounded-lg">
-
-
-          <textarea name="Description" ref={register({ required: true })} />
-
-          <input type="submit" />
-        </div>
       </form>
     </Presence>
   );
