378e949236696293a4e2baf88b1a39cf7a93b2c5
Tom <tom@bearjam.dev>
2020-04-18 17:00:18 +0100

    added lodash plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-lodash`,
     `gatsby-plugin-postcss`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
